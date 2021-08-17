fd4f2b191a3c0d2cab9072f33aabcb463b59ac15
Pandy Knight (@AutomationPanda) <andylpk.247@gmail.com>
2021-08-17 02:04:38 -0400

    Added more initial devices to db

--- registry.py
+++ registry.py
@@ -16,14 +16,30 @@ def init_db():
     db.drop_all()
     db.create_all()
 
-    device = Device(
+    light = Device(
+      name='Front Porch Light',
+      location='Front Porch',
+      type='Light Switch',
+      model='GenLight 64B',
+      serial_number='GL64B-99987')
+
+    thermostat = Device(
       name='Main Thermostat',
       location='Living Room',
       type='Thermostat',
       model='ThermoBest 3G',
       serial_number='TB3G-12345')
     
-    db.session.add(device)
+    fridge = Device(
+      name='Family Fridge',
+      location='Kitchen',
+      type='Refrigerator',
+      model='El Gee Mondo21',
+      serial_number='LGM-20201')
+
+    db.session.add(light)
+    db.session.add(thermostat)
+    db.session.add(fridge)
     db.session.commit()
     
     click.echo('Initialized the database with fresh data.')
