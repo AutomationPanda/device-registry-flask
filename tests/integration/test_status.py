2018fec882b88f13955adcfd83daaf23436ee198
Pandy Knight (@AutomationPanda) <andylpk.247@gmail.com>
2021-09-06 10:35:38 -0400

    Moved status method tests

--- tests/integration/test_status.py
+++ tests/integration/test_status.py
@@ -1,5 +1,10 @@
 """
 This module contains integration tests for the '/status/' resource.
+The main method for '/status/' is GET.
+Flask also automatically adds HEAD and OPTIONS responses for GET routes.
+Unsupported methods should return 405 status codes.
+These methods should be tested, but the tests are repetitive.
+Tests should be parametrized together to minimize code duplication.
 """
 
 # --------------------------------------------------------------------------------
@@ -11,7 +16,7 @@ import requests
 
 
 # --------------------------------------------------------------------------------
-# Tests
+# Tests for GET
 # --------------------------------------------------------------------------------
 
 def test_status_get(base_url):
@@ -22,4 +27,86 @@ def test_status_get(base_url):
   assert response.status_code == 200
   assert data['online'] == True
   assert data['uptime'] > 0
+
+
+# --------------------------------------------------------------------------------
+# Tests for HEAD
+# --------------------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+  'resource',
+  [
+    ('/status/')
+  ]
+)
+def test_head(base_url, resource):
+
+  # Call HEAD
+  url = base_url.concat(resource)
+  response = requests.head(url)
   
+  # Response should be successful without a body
+  assert response.status_code == 200
+  assert response.text == ''
+
+  # Response headers should match GET responses
+  get_response = requests.get(url)
+  assert len(response.headers) == len(get_response.headers)
+  for header in response.headers:
+    assert header in get_response.headers
+    if header != 'Date' and header != 'Content-Length':
+      assert response.headers[header] == get_response.headers[header]
+
+
+# --------------------------------------------------------------------------------
+# Method Tests for OPTIONS
+# --------------------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+  'resource, supported',
+  [
+    ('/status/', ['HEAD', 'OPTIONS', 'GET'])
+  ]
+)
+def test_status_options(base_url, resource, supported):
+
+  # Call OPTIONS
+  url = base_url.concat(resource)
+  response = requests.options(url)
+  
+  # Response should be successful without a body
+  assert response.status_code == 200
+  assert response.text == ''
+
+  # Response 'Allow' header should list supported methods
+  for valid_method in supported:
+    assert valid_method in response.headers['Allow']
+
+
+# --------------------------------------------------------------------------------
+# Tests for Unsupported Methods
+# --------------------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+  'resource, method, supported',
+  [
+    ('/status/', 'DELETE', ['HEAD', 'OPTIONS', 'GET']),
+    ('/status/', 'PATCH', ['HEAD', 'OPTIONS', 'GET']),
+    ('/status/', 'POST', ['HEAD', 'OPTIONS', 'GET']),
+    ('/status/', 'PUT', ['HEAD', 'OPTIONS', 'GET'])
+  ]
+)
+def test_status_invalid_method(base_url, resource, method, supported):
+
+  # Call the unsupported method
+  url = base_url.concat(resource)
+  response = requests.request(method, url)
+  data = response.json()
+
+  # Response should be a 405 error
+  assert response.status_code == 405
+  assert data['error'] == 'method not allowed'
+
+  # Response should list supported methods
+  for valid_method in supported:
+    assert valid_method in data['valid_methods']
