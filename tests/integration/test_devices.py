0a6dc2eaf23ded6d0c84762294f6325e3d0ffa38
Pandy Knight (@AutomationPanda) <andylpk.247@gmail.com>
2021-10-21 23:56:41 -0400

    Added waiting to test_devices.py

--- tests/integration/test_devices.py
+++ tests/integration/test_devices.py
@@ -9,10 +9,11 @@ However, it remains in this project to serve as example code.
 # --------------------------------------------------------------------------------
 
 import requests
+import time
 
 
 # --------------------------------------------------------------------------------
-# Create Tests
+# Tests
 # --------------------------------------------------------------------------------
 
 def test_create_device(base_url, session):
@@ -47,5 +48,47 @@ def test_create_device(base_url, session):
   assert get_response.status_code == 200
   get_devices = [d for d in get_data['devices']
                    if d['id'] == post_data['id']]
+  assert len(get_devices) == 1
+  assert get_devices[0] == post_data
+
+
+def test_create_device_with_waiting(base_url, session):
+
+  # This test is a copy of the test_create_device.
+  # The "create" steps are identical.
+  # The "retrieve" steps, however, actively wait for the device.
+
+  thermostat_data = {
+    'name': 'Main Thermostat',
+    'location': 'Living Room',
+    'type': 'Thermostat',
+    'model': 'ThermoBest 3G',
+    'serial_number': 'TB3G-12345'
+  }
+
+  device_url = base_url.concat('/devices/')
+  post_response = session.post(device_url, json=thermostat_data)
+  post_data = post_response.json()
+  
+  assert post_response.status_code == 200
+  assert 'id' in post_data
+  assert isinstance(post_data['id'], int)
+  thermostat_data['id'] = post_data['id']
+  thermostat_data['owner'] = session.auth[0]
+  assert post_data == thermostat_data
+
+  attempts = 60
+  get_devices = []
+
+  while len(get_devices) == 0 and attempts > 0:
+    attempts -= 1
+    time.sleep(1)
+    get_response = session.get(device_url)
+    get_data = get_response.json()
+    get_devices = [d for d in get_data['devices']
+                     if d['id'] == post_data['id']]
+  
+  assert attempts > 0
+  assert get_response.status_code == 200
   assert len(get_devices) == 1
   assert get_devices[0] == post_data
